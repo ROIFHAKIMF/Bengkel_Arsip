2baec1e7f739facbbb08080633010fb82f3dbb61
MWRaditya <111202314948@mhs.dinus.ac.id>
2025-06-26 12:36:37 +0700

    pertambahan user seeder

--- app/Database/Seeds/UserSeeder.php
+++ app/Database/Seeds/UserSeeder.php
@@ -8,6 +8,24 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-        //
+                // membuat data
+        $data = [
+            [
+                'username' => 'Adit',
+                'password'  => 123456,
+                'role' => 'guest',
+            ],
+            [
+                'username' => 'Rafi',
+                'password'  => 123456,
+                'role' => 'guest',
+            ],
+            [
+                'username' => 'Roif',
+                'password'  => 123456,
+                'role' => 'admin',
+            ],
+        ];
+        $this->db->table('user')->insert($data);
     }
 }
