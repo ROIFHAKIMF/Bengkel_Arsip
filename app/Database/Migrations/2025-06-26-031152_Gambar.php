5c760ff1ccc21cff4c16c12ed6ee05af72d27a6b
MWRaditya <111202314948@mhs.dinus.ac.id>
2025-06-26 10:24:55 +0700

    tambah tabel gambar

---
+++ app/Database/Migrations/2025-06-26-031152_Gambar.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Gambar extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ],
+            'gambar' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => FALSE,
+            ],
+
+        ]);
+        
+        $this->forge->addKey('id', TRUE);
+        $this->forge->createTable('gambar');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('gambar');
+    }
+}
+
+
+
+
