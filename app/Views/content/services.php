b91594bc50857ada82ee6c0b511f7ae3e35dd4a8
MWRaditya <111202314948@mhs.dinus.ac.id>
2025-06-29 19:16:08 +0700

    memperbaiki CRUD hapus pada service

--- app/Views/content/services.php
+++ app/Views/content/services.php
@@ -3,33 +3,16 @@
     <h1 class="fw-bold color-hijau pb-3 text-center">SERVICES BENGKEL ARSIP</h1>
 
     <?php if (session()->get('isLoggedIn')): ?>
-      <div class="row w-50 gap-3">
-        <!-- Tombol Hapus -->
-        <button type="button" 
-                class="btngallery col-3 mb-4" 
-                data-bs-toggle="modal" 
-                data-bs-target="#hapusModal">
-          Hapus
-        </button>
-
-        <!-- Tombol Tambah -->
-        <button type="button" 
-                class="btngallery col-3 mb-4" 
-                data-bs-toggle="modal" 
-                data-bs-target="#addModal">
-          Tambah
-        </button>
-
-        <!-- Tombol Edit -->
-        <button type="button" 
-                class="btngallery col-3 mb-4" 
-                onclick="window.location.href='<?= site_url('service/edit/' . (isset($data_services[0]['id']) ? $data_services[0]['id'] : '')) ?>'">
-          Edit
+      <div class="row w-50 gap-3 mb-4 justify-content-center">
+        <button type="button" class="btngallery col-3" data-bs-toggle="modal" data-bs-target="#hapusModal">Hapus</button>
+        <button type="button" class="btngallery col-3" data-bs-toggle="modal" data-bs-target="#addModal">Tambah</button>
+        <button type="button" class="btngallery col-3">
+          <a href="<?= site_url('service/edit/' . (isset($data_services[0]['id']) ? $data_services[0]['id'] : '')) ?>" style="text-decoration: none; color: inherit;">Edit</a>
         </button>
       </div>
     <?php endif; ?>
 
-    <div class="row gap-5">
+    <div class="row gap-5 justify-content-center">
       <?php foreach ($data_services as $service): ?>
         <div class="card-sr col-lg-4 col-md-6 col-sm-12">
           <div class="bg">
@@ -44,7 +27,7 @@
 </section>
 
 <!-- Modal Tambah -->
-<div class="modal fade" id="addModal" tabindex="-1">
+<div class="modal fade" id="addModal" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false">
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
@@ -73,7 +56,7 @@
 </div>
 
 <!-- Modal Hapus Service -->
-<div class="modal fade" id="hapusModal" tabindex="-1" aria-labelledby="hapusModalLabel" aria-hidden="true">
+<div class="modal fade" id="hapusModal" tabindex="-1" aria-labelledby="hapusModalLabel" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false">
   <div class="modal-dialog modal-dialog-centered modal-lg">
     <div class="modal-content">
       <div class="modal-header">
@@ -81,16 +64,29 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Tutup"></button>
       </div>
       <div class="modal-body">
-        <div class="row">
+        <div class="d-flex flex-wrap justify-content-center gap-2">
           <?php foreach ($data_services as $service): ?>
-            <div class="col-md-4 mb-3">
-              <div class="card">
-                <img src="<?= base_url('img/' . $service['title']) ?>" class="card-img-top" alt="Service">
-                <div class="card-body text-center">
-                  <p class="card-text"><?= esc($service['content']) ?></p>
-                  <a href="<?= site_url('service/delete/' . $service['id']) ?>" class="btn btn-danger btn-sm" onclick="return confirm('Yakin ingin menghapus ini?')">Hapus</a>
-                </div>
+            <div class="card text-center d-flex flex-column align-items-center"
+                 style="width: 120px; height: 180px; border: 1px solid var(--warna-utama); border-radius: 8px; padding: 0.4rem; overflow: hidden;">
+              
+              <!-- Gambar -->
+              <img src="<?= base_url('img/' . $service['title']) ?>" 
+                   style="width: 100%; height: 60px; object-fit: cover; border-radius: 6px;" 
+                   alt="Service">
+
+              <!-- Deskripsi Singkat -->
+              <div class="mt-1" 
+                   style="font-size: 0.65rem; text-align: center; max-height: 3em; overflow: hidden; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical;">
+                <?= esc($service['content']) ?>
               </div>
+
+              <!-- Tombol Hapus -->
+              <a href="<?= site_url('service/delete/' . $service['id']) ?>" 
+                 class="btn btn-sm btn-danger w-100 mt-auto py-0"
+                 style="font-size: 0.6rem;" 
+                 onclick="return confirm('Yakin ingin menghapus ini?')">
+                Hapus
+              </a>
             </div>
           <?php endforeach; ?>
         </div>
