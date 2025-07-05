a418143cda82801f036d1a21931ee3fa1a8cf7dc
ROIFHAKIMF <Rhfgamingyt@gmail.com>
2025-07-05 11:35:53 +0700

    mendesign service 1-5

--- app/Views/content/Services/Service1.php
+++ app/Views/content/Services/Service1.php
@@ -1,6 +1,7 @@
 <section id="service">
     <h1>Service 1: Digitalisasi Arsip</h1>
     <div class="service-1">
+        <a class="btn-exit" href="<?= base_url(relativePath: "#service") ?>"><i class="bi bi-x fw"></i></a>
         <div class="img-group row">
             <img src="<?= base_url('img/slide3.jpg') ?>" alt="Digitalisasi Arsip" class="img-fluid col-12">
             <div class="img-mini col-12">
@@ -12,8 +13,12 @@
             <p class="">Digitalisasi arsip adalah proses mengubah dokumen fisik menjadi format digital untuk memudahkan penyimpanan, pencarian, dan akses informasi. Ini membantu dalam melestarikan arsip penting dan mengurangi penggunaan ruang fisik.</p>
             <p class="">Dengan digitalisasi, arsip dapat diakses dengan cepat dan aman, serta mengurangi risiko kerusakan akibat faktor lingkungan seperti kelembaban atau serangan hama. Proses ini juga memungkinkan integrasi dengan sistem manajemen informasi yang lebih luas, sehingga mempermudah pengelolaan arsip dalam organisasi.</p>
             <div class="d-flex flex-row w-50 justify-center align-items-center gap-2">
-                <a class= "btn btn-outline-warning border border-3 border-warning fw-bold w-50" href="<?= base_url(relativePath: "#service") ?>" >Kembali</a>
-                <a class="btn btn-outline-warning border border-3 border-warning fw-bold w-50" href="<?= base_url('admin?service=2#service') ?>">Next &raquo;</a>
+                <a class="btn-services w-50" href="<?= base_url(relativePath: "#service") ?>" >Kembali</a>
+                <?php if (session()->get('isLoggedIn')): ?>
+                    <a class="btn-services w-50" href="<?= base_url('admin?service=2#service') ?>">Next &raquo;</a>
+                <?php else:?>
+                    <a class="btn-services w-50" href="<?= base_url('/?service=2#service') ?>">Next &raquo;</a>
+                <?php endif;?>
             </div>
         </div>
     </div>
