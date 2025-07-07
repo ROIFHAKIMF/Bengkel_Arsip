502f7368920f822c908393b037270e970e7fb37f
ROIFHAKIMF <Rhfgamingyt@gmail.com>
2025-07-07 11:43:21 +0700

    perbaikan mempercantik routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -17,26 +17,24 @@ $routes->get('logout', 'AuthController::logout');
 // Halaman dashboard dan manajemen data (khusus admin)
 $routes->group('admin', ['filter' => 'rolefilter'], function ($routes) {
     $routes->get('/', 'DashboardController::admin');
-
     // Service (Layanan)
     $routes->post('service/tambah', 'DashboardController::tambahService');
     $routes->post('service/edit', 'DashboardController::editService');
     $routes->post('service/hapus', 'DashboardController::hapusService');
-
     // About
     $routes->post('about/tambah', 'DashboardController::tambahAbout');
     $routes->post('about/edit', 'DashboardController::editAbout');
     $routes->post('about/hapus', 'DashboardController::hapusAbout'); 
     
+    $routes->post('client/tambah', 'DashboardController::tambahClient');
+    $routes->post('client/edit', 'DashboardController::editClient');
+    $routes->post('client/hapus', 'DashboardController::hapusClient');
+    // Routes untuk CRUD Gallery
+    $routes->post('gallery/tambah', 'DashboardController::tambahgallery');
+    $routes->post('gallery/edit', 'DashboardController::editgallery');
+    $routes->post('gallery/hapus', 'DashboardController::hapusgallery');
 });
 // Routes untuk CRUD Client
-$routes->post('/admin/client/tambah', 'DashboardController::tambahClient');
-$routes->post('/admin/client/edit', 'DashboardController::editClient');
-$routes->post('/admin/client/hapus', 'DashboardController::hapusClient');
-// Routes untuk CRUD Gallery
-$routes->post('/admin/gallery/tambah', 'DashboardController::tambahgallery');
-$routes->post('/admin/gallery/edit', 'DashboardController::editgallery');
-$routes->post('/admin/gallery/hapus', 'DashboardController::hapusgallery');
 
 $routes->get('/footer', 'DashboardController::footer');
 $routes->post('/update-social-media', 'DashboardController::updateSocialMedia');
